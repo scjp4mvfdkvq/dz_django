27664635e89da5cea200a3ab4f5437f9d30f858a
Alexandr <scjp4mvfdkvq@mail.ru>
2024-12-19 13:33:37 +0300

    Сделал урок 4 (#3)

--- server/apps/tracking/admin.py
+++ server/apps/tracking/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Operation
+from .models import Operation, Category
 
 
 # admin.site.register(Operation)
@@ -10,3 +10,7 @@ class OperationAdmin(admin.ModelAdmin):
     list_filter = ('operation_at',) # фильтр по дате создания
     search_fields = ('name',) # поиск по имени
     ordering = ('-operation_at',) # сортировка по дате создания
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',) # поля для отображения в списке объектов
--- server/apps/tracking/models.py
+++ server/apps/tracking/models.py
@@ -4,25 +4,42 @@ import datetime
 from django.db import models
 
 
+class Category(models.Model):
+
+    name = models.CharField(max_length=64, verbose_name="Название категории")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return f"{self.name}"
+
 class Operation(models.Model):
 
     name = models.TextField(
         verbose_name="Наименование операции",
+        db_index=True
     )
-
     description = models.TextField(
         blank=True,
         null=True,
         verbose_name="Описание операции",
     )
-
     cost = models.FloatField(
         verbose_name="Стоимость",
     )
-
     operation_at = models.DateTimeField(
         default=datetime.datetime.now(),
         verbose_name="Дата операции",
+    )
+    category = models.ForeignKey(
+        Category,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="operations",
+        verbose_name="Категория операции",
     )
 
     class Meta:
@@ -30,7 +47,7 @@ class Operation(models.Model):
         verbose_name_plural = "Операции"
 
     def __str__(self):
-        return f"{self.name}"
+        return f"At-{self.operation_at}-{self.name}"
 
 
 class GoalType(models.TextChoices):
@@ -81,3 +98,11 @@ class GoalManager(models.Manager):
 
     def budgets(self):
         return self.filter(goal_type=GoalType.SPENDING)
+
+class MyModelManager(models.Manager):
+    def my_custom_query(self):
+        return self.filter(some_field='some_value')
+
+class MyModel(models.Model):
+    some_field = models.CharField(max_length=100)
+    objects = MyModelManager()  # присваиваем наш менеджер объекту модели
