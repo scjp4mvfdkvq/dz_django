d2c1b22589e267dcaf6c387ee7fd7a6aac765a4e
Alexandr <scjp4mvfdkvq@mail.ru>
2024-12-19 13:28:31 +0300

    Сделал урок 4

--- server/apps/tracking/views.py
+++ server/apps/tracking/views.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 
 from django.shortcuts import render, get_object_or_404
+from django.db import transaction
 
 from .models import Operation
 from .utils import convert_price_to_eur
 
 
+
+@transaction.atomic
 def index(request):
     # Главная страница
     all_operations = Operation.objects.all()
